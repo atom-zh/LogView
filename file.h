9798f7536731e4a47a47c3bb8d91028594ac378f
atom-zh <xiaolongatom@gmail.com>
2022-08-11 18:52:11 +0800

    add initial source code

---
+++ file.h
@@ -0,0 +1,13 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <QMainWindow>
+#include <QWidget>
+
+class file
+{
+public:
+    file();
+};
+
+#endif // FILE_H
---
+++ mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_aVersion_triggered()
+{
+    QMessageBox::information(this,"版本信息","V0.0.1 by Shannon\n");
+}
+
